39a096600450786ef8a3bacd6c96389caaa778db
RA <rana.akbar.08@gmail.com>
2022-07-31 21:10:39 +0100

    inital

---
+++ Day1_py_printing.py
@@ -0,0 +1,22 @@
+# #Write your code below this line 👇
+
+# print(")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+#Write your code below this line 👇
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+
+
+
+
+
+
+
+
+
